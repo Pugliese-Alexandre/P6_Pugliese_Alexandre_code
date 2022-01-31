ceff728b490b34233d5ecac0af5c3b0240f182e8
Pugliese <Pugliese-Alexandre@live.be>
2022-01-31 21:31:12 +0100

    Rectification du message d'erreur "mot de passe incorrect..."
    Rectification du message d'erreur "mot de passe incorrect..."

--- middleware/checkPassword.js
+++ middleware/checkPassword.js
@@ -4,7 +4,7 @@ const passwordSchema = require('../models/password');
 // On vérifie que le mot de passe correspond au schema.
 module.exports = (req, res, next) => {
     if (!passwordSchema.validate(req.body.password)) {
-               res.writeHead(400, '{"message":"Mot de passe requis : 10 caractères minimun. Au moins 1 Majuscule, 1 minuscule. Sans espaces"}', {
+               res.writeHead(400, '{"message":"Mot de passe requis : 10 caractères minimun. Au moins 1 Majuscule, 1 minuscule et un chiffre. Sans espaces"}', {
             'content-type': 'application/json'
         });
         res.end('Mot de passe incorrect');
